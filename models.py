68a900a546149714e71a0018303e78ce9935cad1
tranhoc22102005-max <tranhoc22102005@gmail.com>
2026-04-17 20:51:09 +0700

    Create models.py

---
+++ models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+from .models import Lesson
+
+class Question(models.fields.CharField):
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+    grade = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+class Submission(models.Model):
+    enrollment = models.ForeignKey('Enrollment', on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
